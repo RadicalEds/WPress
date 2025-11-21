da344e64e28c30b725971f14985a0644d8b9abbf
RadicalEd <241870559+RadicalEds@users.noreply.github.com>
2025-11-20 23:58:39 -0600

    version_1.0

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
-# WPress (W.I.P.)
-Installs and configures a LAMP Server with Wordpress.
+# WPress
+Installs and Configures a LAMP Server with a Wordpress VHost.
 
 ### Requirements
 - An Internet Connection
-- Debian configured with apt
+- Debian with Apt
 - root access for installing
 - A Domain Name already configured with the servers address
 
 Any missing packages will be installed by apt    
+
+### Features
+- Install/Configure LAMP Server with all wordpress features
+- Enables TLS with LetsEncrypt
+- Creates a new MYSQL User/Database (if needed)
+- Installs and Automatically Configures Wordpress
+- URL Rewriting and Permalinks will work without any fuss
+
+### Planned Features
+- Security Tweaks that Every Wordpress Should Have by Default
+- Auto Install/Configure Plugins/Themes
+- The Ability to Modify a Current Installation, Import/Export Pages/Posts/Products, Install/Update Plugins/Themes etc.
+
+### Screenshots
+
+![example](https://cdn.discordapp.com/attachments/1434270963977031690/1441301485777584128/wpress-demo.png?ex=69214c1c&is=691ffa9c&hm=7f1beb20bb9983ca83392f8067345047b2b01f9ff25fcb17d0156f29504d46af&)
--- wpress.sh
+++ wpress.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #####################################
-VERSION="0.1"
+VERSION="1.0"
 NAME="wpress"
 AUTHOR="RadicalEd"
-DESCRIPTION="Installs and configures a LAMP Server with Wordpress."
+DESCRIPTION="Installs and Configures a LAMP Server with a Wordpress VHost."
 LICENSE=""
 PROGRAM=$0
 HIGHLIGHT="cyan"
@@ -12,9 +12,9 @@ HIGHLIGHT="cyan"
 
 DIRECTORY="/var/www"
 DBUSER="groot"
-DBPASS="toorg"
+DBPASS="wpress"
 ADMINUSER="groot"
-ADMINPASS="toorg"
+ADMINPASS="wpress"
 
 # End of Defaults
 #####################################
@@ -45,13 +45,13 @@ c () { # Set/Clear Colors
 }
 
 banner () {
-cat << 'EOF' | sed -e "1s/^/$(c blue)/" -e "4s/$/$(c n)/" -e "5s/^/$(c cyan)/" -e "6s/$/$(c n)/" >&2
-  .::    .   .:::::::::::::. :::::::..  .,:::::: .::::::.  .::::::. 
-  ';;,  ;;  ;;;'  `;;;```.;;;;;;;``;;;; ;;;;'''';;;`    ` ;;;`    ` 
-   '[[, [[, [['    `]]nnn]]'  [[[,/[[['  [[cccc '[==/[[[[,'[==/[[[[,
-     Y$c$$$c$P      $$$""     $$$$$$c    $$""""   '''    $  '''    $
-      "88"888       888o      888b "88bo,888oo,__88b    dP 88b    dP
-       "M "M"       YMMMb     MMMM   "W" """"YUMMM"YMmMY"   "YMmMY" 
+cat << 'EOF' | cut -c1-$(tput cols) | sed -e "1s/^/$(c blue)/" -e "4s/$/$(c n)/" -e "5s/^/$(c cyan)/" -e "6s/$/$(c n)/" -e 's/   / . /g' -e 's/   / + /g' >&2
+  .::    .   .:::::::::::. :::::::..  .,:::::: .::::::.  .::::::.   
+  ';;,  ;;  ;;;'`;;;```.;;;;;;;``;;;; ;;;;'''';;;`    ` ;;;`    `   
+   '[[, [[, [['  `]]nnn]]'  [[[,/[[['  [[cccc '[==/[[[[,'[==/[[[[,  
+     Y$c$$$c$P    $$$""     $$$$$$c    $$""""   '''    $  '''    $  
+      "88"888     888o      888b "88bo,888oo,__88b    dP 88b    dP  
+       "M "M"     YMMMb     MMMM   "W" """"YUMMM"YMmMY"   "YMmMY"   
 EOF
 }
 
@@ -70,13 +70,13 @@ $(echo -n "	$DESCRIPTION" | fmt -w $(tput cols))
 $(c $HIGHLIGHT)USAGE$(c n): $PROGRAM [options] <working-domain.com>
 
 	-h            : show usage
-	-v            : enable debugging messages
 	-x <path>     : install site to this path     ($DIRECTORY)
 	-n <name>     : name of the database to use   (same as domain)
 	-u <user>     : database user to setup/assign ($DBUSER)
 	-p <pass>     : password for the db user      ($DBPASS)
 	-U <User>     : New Admin Username            ($ADMINUSER)
 	-P <Password> : New Admin Password            ($ADMINPASS)
+	-e <email>    : Email for both Cert & WPAdmin (webmaster@your-domain)
 
 EOF
 }
@@ -90,9 +90,11 @@ error () {
 	exit "$code"
 }
 
-hr () { # Horizontal Rule
+hr () { # Horizontal Rule the length of the banner
 	character="${1:--}"
-	printf -v _hr "%*s" $(tput cols) && echo "${_hr// /$character}";
+	twidth=$(tput cols)
+	bwidth=$(banner 2>&1 | wc -L)
+	printf -v _hr "%*s" $((twidth<bwidth-3 ? twidth : bwidth-3)) && echo "${_hr// /$character}";
 }
 clearline () {
 	printf "\r%*s\r" $(tput cols);
@@ -115,33 +117,35 @@ spinner () { # $1=pid $*=text
 
 	while ps -p $pid >/dev/null;do
 		clearline >&2
-		printf "\r	$(c yellow)[${symbols[$((idx % len))]}]$(c n) $*" >&2
+		printf "\r  $(c yellow)[${symbols[$((idx % len))]}]$(c n) $*" >&2
 		idx=$((idx+1))
-		sleep 0.3
+		sleep 0.1
 	done
 	clearline >&2
 }
 
 blocksay() { # $1=color $*=text
 	color="$1"
+	symbol=${BLOCKSAYSYMBOL:-+}
 	shift
-	echo "	$(c $color)[+]$(c n) $*" >&2
+	echo "  $(c $color)[$symbol]$(c n) $*" >&2
 }
 
 # End of Printing Functions
 #####################################
 # Arguments
 
-while getopts "hvx:n:u:p:U:P:" o;do
+while getopts "hvx:n:u:p:U:P:e:" o;do
 	case "${o}" in
-		(h) usage && exit      ;;
-		(v) DEBUG="true"       ;;
-		(x) DIRECTORY="$OPTARG";;
-		(n) DBNAME="$OPTARG"   ;;
-		(u) DBUSER="$OPTARG"   ;;
-		(p) DBPASS="$OPTARG"   ;;
-		(U) ADMINUSER="$OPTARG";;
-		(P) ADMINPASS="$OPTARG";;
+		(h) usage && exit          ;;
+		(v) DEBUG="true"           ;;
+		(x) DIRECTORY="$OPTARG"    ;;
+		(n) DBNAME="$OPTARG"       ;;
+		(u) DBUSER="$OPTARG"       ;;
+		(p) DBPASS="$OPTARG"       ;;
+		(U) ADMINUSER="$OPTARG"    ;;
+		(P) ADMINPASS="$OPTARG"    ;;
+		(e) CERTMAIL="$OPTARG"     ;;
 		(*) echo "Try Using $PROGRAM -h for Help And Information" >&2 && exit 1;;
 	esac
 done
@@ -165,10 +169,13 @@ shift $((OPTIND-1))
 
 DIRECTORY="${DIRECTORY:-/var/www}"
 DBNAME="${DBNAME:-$DOMAIN}"
+DBNAME="$(echo -n "$DBNAME" | sed 's/[^A-Za-z0-9]/_/g')"
 DBUSER="${DBUSER:-groot}"
-DBPASS="${DBPASS:-toorg}"
+DBPASS="${DBPASS:-wpress}"
 ADMINUSER="${ADMINUSER:-groot}"
-ADMINPASS="${ADMINPASS:-toorg}"
+ADMINPASS="${ADMINPASS:-wpress}"
+LOGDIR="${LOGDIR:-/var/log/apache2}"
+CERTMAIL="${CERTMAIL:-webmaster@$DOMAIN}"
 
 # End Of Variables
 #####################################
@@ -180,79 +187,202 @@ ADMINPASS="${ADMINPASS:-toorg}"
 #####################################
 # Functions
 
+success () {
+	echo 0 > /tmp/$$.tmpfile
+}
+failure () {
+	rv=${1:-1}
+	echo "$rv" > /tmp/$$.tmpfile
+	return "$rv"
+}
+cleanup () {
+	erm () { # remove a file/directory if it exists
+		[ -e "$1" ] && rm -rf "$1" 2> /dev/null 1> /dev/null
+	}
+	erm "$DIRECTORY/$DOMAIN/docroot/firewall.test"
+	erm /tmp/$$.tmpfile
+	erm /tmp/wordpress.tar.gz
+}
+evaluate () {
+	rv=$(cat /tmp/$$.tmpfile)
+	rm /tmp/$$.tmpfile
+	if [ "$rv" -eq 0 ];then
+		blocksay green "$1"
+	else
+		BLOCKSAYSYMBOL="-" blocksay red "$2 (Code: $?)"
+	fi
+	return $rv
+}
+close () {
+	cleanup
+	exit "${1:-0}"
+}
+print_vhost () {
+cat << EOF
+<VirtualHost *:80>
+	ServerName $DOMAIN
+	ServerAdmin webmaster@localhost
+
+	DocumentRoot $DIRECTORY/$DOMAIN/docroot
+
+	<Directory $DIRECTORY/$DOMAIN/docroot>
+		Options FollowSymLinks
+		AllowOverride Limit Options FileInfo
+		DirectoryIndex index.php
+		Require all granted
+	</Directory>
+
+	<Directory $DIRECTORY/$DOMAIN/wp-content>
+		Options FollowSymLinks
+		Require all granted
+	</Directory>
+
+	ErrorLog $LOGDIR/$DOMAIN/error.log
+	CustomLog $LOGDIR/$DOMAIN/access.log combined
+
+</VirtualHost>
+EOF
+}
+
 install_packages() {
 	PACKAGES=(
-		'curl'               # To Retrieve Wordpress
-		'unzip'              # To Unzip Wordpress
-		'certbot'            # For Setting up TLS
-		'apache2'            # Our Server
-		'mariadb-server'     # Our Database
-		'php'                # Our Language
-		'libapache2-mod-php' # Teach Server our Language
-		'ghostscript'        # Wordpress Dependency 
-		'php-mysql'          # Wordpress Dependency
-		'php-zip'            # Wordpress Dependency
-		'php-xml'            # Wordpress Dependency
-		'php-mbstring'       # Wordpress Dependency
-		'php-json'           # Wordpress Dependency
-		'php-intl'           # Wordpress Dependency
-		'php-imagick'        # Wordpress Dependency
-		'php-curl'           # Wordpress Dependency
-		'php-bcmath'         # Wordpress Dependency
-	)
-	apt install -y ${PACKAGES[@]} 1>/dev/null 2>/dev/null &
+		'curl'                   # To Retrieve Wordpress
+		'tar'                    # To Decompress Wordpress
+		'certbot'                # For Setting up TLS
+		'python3-certbot-apache' # For Setting up TLS
+		'apache2'                # Our Server
+		'mariadb-server'         # Our Database
+		'php'                    # Our Language
+		'libapache2-mod-php'     # Teach Server our Language
+		'ghostscript'            # Wordpress Dependency 
+		'php-mysql'              # Wordpress Dependency
+		'php-zip'                # Wordpress Dependency
+		'php-xml'                # Wordpress Dependency
+		'php-mbstring'           # Wordpress Dependency
+		'php-json'               # Wordpress Dependency
+		'php-intl'               # Wordpress Dependency
+		'php-imagick'            # Wordpress Dependency
+		'php-curl'               # Wordpress Dependency
+		'php-bcmath'             # Wordpress Dependency
+	);
+	(
+		apt update 2>/dev/null 1>/dev/null || failure || exit
+		apt install -y ${PACKAGES[@]} 1>/dev/null 2>/dev/null || failure || exit
+
+		# install wp-cli, a php commandline library that performs common wordpress tasks
+		if [ ! -e "/usr/local/bin/wp" ];then
+			curl --silent https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp 2> /dev/null || failure || exit
+			chmod +x "/usr/local/bin/wp"
+		fi
+		success
+	) &
 	spinner $! "Installing Packages..."
-	blocksay green "Packages Installed"
-	# need a way to tell if install failed
+	evaluate "Packages Installed" "Apt Error" || close $?
 }
 
 check_services() {
 	(
-		echo "" >/dev/null
+		systemctl restart apache2 || failure || exit
+		systemctl restart mysql || failure || exit
+		success
 	) &
-	spinner $! "Installing Wordpress..."
-	blocksay green "Services Are Running"
+	spinner $! "Restarting Services..."
+	evaluate "Services Are Running" "There Was a Problem Starting the Services" || close $?
 }
 check_firewall() {
 	(
-		echo "" >/dev/null
+		if which ufw >/dev/null;then
+			ufw allow http > /dev/null 2> /dev/null
+			ufw allow https > /dev/null 2> /dev/null
+		fi
+		testfile="$DIRECTORY/$DOMAIN/docroot/firewall.test"
+		date +%s > "$testfile"
+		curl --silent "http://$DOMAIN/firewall.test" > /dev/null 2> /dev/null || failure || exit
+		rm "$testfile"
+		success
 	) &
 	spinner $! "Checking Firewall..."
-	blocksay green "Firewall is Ok"
+	evaluate "Firewall is allowing traffic" "There was an Error with the Firewall" || close $?
 }
 check_domain() {
 	(
-		echo "" >/dev/null
+		if which dig > /dev/null;then
+			# check with dig
+			[ "$(dig $DOMAIN +short)" ] || failure || exit
+		else
+			# check with ping
+			ping -c1 $DOMAIN > /dev/null 2> /dev/null || failure || exit
+		fi
+		success
 	) &
 	spinner $! "Checking Domain..."
-	blocksay green "Domain Is Working"
+	evaluate "The Domain is Working" "Could not resolve the domain" || close $?
 }
-create_site_tree() {
+create_directories() {
 	(
-		echo "" >/dev/null
+		if [ ! -e "$DIRECTORY/$DOMAIN/docroot" ];then
+			mkdir -p "$DIRECTORY/$DOMAIN/docroot" || failure || exit # Site Root
+		fi
+		if [ ! -e "$LOGDIR/$DOMAIN" ];then
+			mkdir -p "$LOGDIR/$DOMAIN/" || failure || exit # For Log Files
+			touch "$LOGDIR/$DOMAIN/access.log" # so vhost will find it
+			touch "$LOGDIR/$DOMAIN/error.log"  # so vhost will find it
+			chgrp -hR adm "$LOGDIR/$DOMAIN/" || failure || exit # So Server Can Write Logs
+		fi
+		success
 	) &
-	spinner $! "Installing Wordpress..."
+	spinner $! "Creating Directory Structure..."
+	evaluate "Directories Created" "There was an Issue creating Directories" || close $?
 }
 install_wordpress() {
 	(
-		echo "" >/dev/null
+
+		# get wordpress
+		curl --silent https://wordpress.org/latest.tar.gz >> /tmp/wordpress.tar.gz || failure || exit
+		tar -xzf /tmp/wordpress.tar.gz -C "$DIRECTORY/$DOMAIN/" 2>/dev/null 1>/dev/null || failure || exit
+		rm -rf "$DIRECTORY/$DOMAIN/docroot" "/tmp/wordpress.tar.gz" 2> /dev/null 1> /dev/null || failure || exit
+		mv "$DIRECTORY/$DOMAIN/wordpress" "$DIRECTORY/$DOMAIN/docroot" 2> /dev/null 1> /dev/null || failure || exit
+		chown -R www-data:www-data "$DIRECTORY/$DOMAIN"
+
+		# setup wordpress
+		cd "$DIRECTORY/$DOMAIN/docroot"
+		sudo -u www-data /usr/local/bin/wp core config --dbname="$DBNAME" --dbuser="$DBUSER" --dbpass="$DBPASS" 2> /dev/null 1> /dev/null || failure || exit
+		sudo -u www-data /usr/local/bin/wp core install --url="https://$DOMAIN/" --title="$DOMAIN" --admin_user="$ADMINUSER" --admin_password="$ADMINPASS" --admin_email="$CERTMAIL" 2> /dev/null 1> /dev/null || failure || exit
+
+		success
 	) &
 	spinner $! "Installing Wordpress..."
-	blocksay green "Wordpress Installed"
+	evaluate "Wordpress Installed" "Something went wrong while installing Wordpress" || close $?
 }
 install_vhost() {
 	(
-		echo "" >/dev/null
+		sdir="/etc/apache2/sites-available"
+		print_vhost > "$sdir/$DOMAIN.conf" || failure || exit
+		a2ensite $DOMAIN.conf 2>/dev/null 1>/dev/null || failure || exit
+		a2enmod rewrite 2>/dev/null 1>/dev/null || failure || exit
+		success
 	) &
-	spinner $! "Creating Vhost..."
-	blocksay green "VHost Created"
+	spinner $! "Creating VHost..."
+	evaluate "VHost Created" "Error when creating VHost" || close $?
 }
 install_cert() {
 	(
-		echo "" >/dev/null
+		certbot -d $DOMAIN -m $CERTMAIL --reinstall --agree-tos --no-eff-email 2>/dev/null 1>/dev/null || failure || exit
+		success
 	) &
 	spinner $! "Installing Certificate With Certbot..."
-	blocksay green "Certificate Installed"
+	evaluate "Certificate Installed" "Error while installing Certificate" || close $?
+}
+setup_database() {
+	(
+		mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DBNAME;" 2>/dev/null 1>/dev/null || failure || exit
+		mysql -u root -e "CREATE USER IF NOT EXISTS '$DBUSER'@'localhost' IDENTIFIED BY '$DBPASS';" 2>/dev/null 1>/dev/null || failure || exit
+		mysql -u root -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'localhost';" 2>/dev/null 1>/dev/null || failure || exit
+		mysql -u root -e "FLUSH PRIVILEGES;" 2>/dev/null 1>/dev/null || failure || exit
+		success
+	) &
+	spinner $! "Building Database..."
+	evaluate "Database Configured" "Error while Configuring Database" || close $?
 }
 
 
@@ -260,38 +390,51 @@ install_cert() {
 #####################################
 # Execution
 
+trap 'close' SIGINT
+
 	# Display Fancy Banner
 clear
 echo >&2
 banner >&2
 echo >&2
-echo "	$DESCRIPTION" >&2
-hr >&2
+echo "    $DESCRIPTION" | fmt -w $(tput cols) >&2
+hr | sed -e 's/^../ </g' -e 's/..$/> /g' >&2
 
 	# Display Details
 echo >&2
-echo "	Domain:   $DOMAIN" >&2
-echo "	Path:     $DIRECTORY/$DOMAIN" >&2
-echo "	Admin:    $ADMINUSER" >&2
-echo "	DB User:  $DBUSER" >&2
-echo "	Database: $DBNAME" >&2
+cat << EOF >&2
+	Domain:   $DOMAIN
+	Admin:    $ADMINUSER/$ADMINPASS
+	DB User:  $DBUSER/$DBPASS
+	Database: $DBNAME
+	Sitepath: $DIRECTORY/$DOMAIN
+	Logpath:  $LOGDIR/$DOMAIN
+EOF
 echo >&2
-hr >&2
-echo >&2
+hr | sed -e 's/^../ </g' -e 's/..$/> /g' >&2
 
 	# Do The Things
+echo >&2
+check_domain
 install_packages
-create_site_tree
+create_directories
 install_vhost
 check_services
 check_firewall
-check_domain
 install_cert
+setup_database
 install_wordpress
-
+echo >&2
+hr | sed -e 's/^../ </g' -e 's/..$/> /g' >&2
+echo >&2
+cat << EOF >&2
+	$(c green)Wordpress Successfully Installed$(c n)
+	$(c cyan)https://$DOMAIN/$(c n)
+EOF
+echo >&2
+close
 
 
 
 # End of Execution
 #####################################
-sleep 1
